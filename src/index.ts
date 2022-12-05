317a0b887c77d4e74bb0fcb485771ef49b837e0f
Rich Plastow <rich@loop.coop>
2022-12-05 10:41:43 +0700

    normalises src/ indentation to 4 spaces

--- src/index.ts
+++ src/index.ts
@@ -6,44 +6,46 @@ import type { RollupCommentSwapOptions } from '../types';
 import quick from './quick';
 import slow from './slow';
 import {
-  codeContainsCssJsCommentSwap,
-  codeContainsHtmlCommentSwap,
-  pathToFiletype,
+    codeContainsCssJsCommentSwap,
+    codeContainsHtmlCommentSwap,
+    pathToFiletype,
 } from './utils';
 
-export default function commentSwap(opts: RollupCommentSwapOptions = {}): Plugin {
-  opts = {
-    quick: true,
-    ...opts,
-  };
-
-  return {
-    // `name` is used by Rollup for error messages and warnings.
-    name: 'commentSwap',
-
-    transform(
-      code, // the source code of a given file, as a string
-      id, // the path that was used to import the module, as a string, eg './utilities.js'
-    ) {
-      const filetype = pathToFiletype(id);
-
-      // pathToFiletype() will return `Other` if `id` is not an extension we
-      // can transform. Returning `null` tells Rollup that this file does not
-      // need to be transformed.
-      if (filetype === Filetype.Other) return null;
-
-      // Only process HTML files which contain at least one of these strings:
-      //     <!--=   =-->   <!--$   $-->   ?-->   <!--:
-      // Only process CSS and JS files which contain at least one of these strings:
-      //     /*=   =*/   /*$   $*/   ?*/   /*:
-      // At least one of these will be present if the file uses Comment Swaps.
-      if (filetype === Filetype.Html) {
-        if (! codeContainsHtmlCommentSwap(code)) return null;
-      } else {
-        if (! codeContainsCssJsCommentSwap(code)) return null;
-      }
-
-      return opts.quick ? quick(code, filetype) : slow(code);
-    }
-  };
+export default function commentSwap(
+    opts: RollupCommentSwapOptions = {},
+): Plugin {
+    opts = {
+        quick: true,
+        ...opts,
+    };
+  
+    return {
+        // `name` is used by Rollup for error messages and warnings.
+        name: 'commentSwap',
+    
+        transform(
+            code, // the source code of a given file, as a string
+            id, // the path which imported the module, as a string, eg './foo.js'
+        ) {
+            const filetype = pathToFiletype(id);
+      
+            // pathToFiletype() will return `Other` if `id` is not an extension
+            // we can transform. Returning `null` tells Rollup that this file does
+            // not need to be transformed.
+            if (filetype === Filetype.Other) return null;
+      
+            // Only process HTML files which contain at least one of these strings:
+            //     <!--=   =-->   <!--$   $-->   ?-->   <!--:
+            // Only process CSS and JS which contain at least one of these strings:
+            //     /*=   =*/   /*$   $*/   ?*/   /*:
+            // At least one of these will be present if the file uses Comment Swaps.
+            if (filetype === Filetype.Html) {
+                if (! codeContainsHtmlCommentSwap(code)) return null;
+            } else {
+                if (! codeContainsCssJsCommentSwap(code)) return null;
+            }
+      
+            return opts.quick ? quick(code, filetype) : slow(code);
+        }  
+    };
 }
--- src/quick/quickCss.ts
+++ src/quick/quickCss.ts
@@ -1,3 +1,5 @@
 export default function quickCss(code: String) {
+    const commentPairs = [];
+
     return `${code}\n\n// @TODO quickCss`;
 }
